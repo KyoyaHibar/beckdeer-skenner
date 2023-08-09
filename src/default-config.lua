f95cac25afb36df82fbb258326ad672b84898f43
KyoyaHibar <138785580+KyoyaHibar@users.noreply.github.com>
2023-08-09 12:37:53 +0800

    Update default-config.lua

--- src/default-config.lua
+++ src/default-config.lua
@@ -13,7 +13,7 @@ return {
 	-- $scripts that executes after backdoor is found
 	-- $you can add any scripts here
 	["autoExec"] = {
-		[[print("Kyo was here")]],
+		[[print("%username% was here")]],
 	},
 
 	-- $remote filters that you don't want to be scanned
